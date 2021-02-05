98faf9d5618b61d804461ebb3df730dfca7308a4
JorgeCab2711 <60406863+JorgeCab2711@users.noreply.github.com>
2021-02-05 17:26:45 -0600

    Cambios en el main

--- CalculadoraGeneral.java
+++ CalculadoraGeneral.java
@@ -5,5 +5,6 @@ Clase: Estructuras de datos.
 Tarea: Calculadora generica
 */
 public interface CalculadoraGeneral {
+    
     public String calculo(String expresion);
 }
--- Main.java
+++ Main.java
@@ -4,8 +4,31 @@ Carnet: 20009
 Clase: Estructuras de datos.
 Tarea: Calculadora generica
 */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println();
+        //Try/Catch para proteger de que el archivo no exista
+        try{
+            //Lector del archivo y archivo
+            File archivo = new File("Datos.txt");
+            Scanner lector = new Scanner(archivo);
+            //Si el archivo llamado "Datos.txt" existe
+            if (archivo.exists()){
+                //Imprimir nombre del archivo
+                System.out.println("Nombre de archivo: "+ archivo.getName());
+                //mientras el archivo tenga lineas que leer imprimirá las lineas leidas
+                while (lector.hasNextLine()){
+                    String data = lector.nextLine();
+                    System.out.println(data);
+                }
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("El archivo no existe o no está en la misma carpeta.");
+        }
     }
 }
